31729acb89464e43ff59157c427b29a220ec2dab
abc <liufangjin156@163.com>
2023-09-18 09:32:07 +0800

    增加其他示例，删除无效依赖

---
+++ src/views/openSourceChina/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "index"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped lang="less">
+
+</style>
